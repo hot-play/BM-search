000396bf0aceef9a5513c8fcec6a358c2c490bac
hot-play <usov2002@yandex.ru>
2022-05-09 16:06:33 +0700

    Finish

--- BM-search/BM-search.cpp
+++ BM-search/BM-search.cpp
@@ -1,20 +1,61 @@
-﻿// BM-search.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
 
-#include <iostream>
+using namespace std;
+const int SymbolNumber = 256;//Константное число, для создания массива со сдвигами
 
-int main()
+//Предтрансляция
+void badChar(string pattern, int patternArray[SymbolNumber]) //Формируем массив для шаблон-строки(Предтрансляция)
 {
-    std::cout << "Hello World!\n";
+	int patternSize = pattern.length();
+	for (int i = 0; i < SymbolNumber; i++)//В начале заполняем максимальной длинной сдвига
+		patternArray[i] = patternSize;
+	for (int i = 0; i < patternSize - 1; i++)//Для символов, которые есть в шаблоне подсчитываем сдвиг до конца шиблона
+		patternArray[(int)pattern[i]] = patternSize - 1 - i;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+//БМ поиск
+int search(string str, string pattern) {
+	int strSize = str.length();//Длина строки, в которой ищем
+	int patternSize = pattern.length();//Длина строки, которую ищем (шаблон)
+	int *patternArray;//Массив для подсчет индекса, накоторый будем сдвигать
+	badChar(pattern, patternArray);//Придтрансляция
+	for (int i = 0; i <= strSize - patternSize; i += patternArray[(int)str[i]]) //С помощью массива ищем совпадения и сдвигаем на определенное количество символов
+	{
+		int k = i; // Идет по строке, в которой ищем
+		int j = patternSize - 1; // Идет по строке, которую ищем
+		for (j = patternSize - 1; j >= 0; j--)
+			if (str[k] == pattern[j])
+				k--;
+			else //В случае, если не совпало, то возвращаемся в первый цикл
+			{
+				j = patternSize - 1;
+				break;
+			}
+		if (j < 0) //Возвращаем индекс с которого начинается совпадение
+		{
+			delete[] patternArray;
+			return i + 1 - patternSize;
+		}
+	}
+	delete[] patternArray;
+	return -1;//Если не нашли, то возвращаем -1
+}
+int main() {
+	setlocale(LC_ALL, "Rus");
+	string str;
+	cout << "Строка, в которой ищем :";
+	cin >> str;
+	string pattern;
+	cout << "Строка, которую ищем (шаблон) :";
+	cin >> pattern;
+	int result = -1;
+	result = search(str, pattern);
+	if (result != -1)
+		cout << endl << "Вхождение было обнаружено в " << result << " индексе" << endl;
+	else
+		cout << endl << "Вхождений обнаружено не было" << endl;
+	system("pause");
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
